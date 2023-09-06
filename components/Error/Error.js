8f2a8a1ab8b965f9371f73ce3150e5fe92157818
Dmitrii <stepanov.dmitrii.spo@gmail.com>
2023-09-06 17:55:26 +0400

    first commit

---
+++ components/Error/Error.js
@@ -0,0 +1,17 @@
+class Error {
+  render() {
+    const html = `
+    <div class="error-container">
+      <div class="error-message">
+        <h3>Error</h3>
+        <p>Try to come back later</p>
+      </div>
+    </div>
+    
+    `;
+
+    ROOT_ERROR.innerHTML = html;
+  }
+}
+
+const errorPage = new Error();
---
+++ readme.md
@@ -0,0 +1,3 @@
+Course: https://www.youtube.com/playlist?list=PL7cTIfGFrdKn9lnGXEUWEXYdYp-HKGPAw
+
+To open the web-page, you need to write the command "http-server" in the console in the root folder.
